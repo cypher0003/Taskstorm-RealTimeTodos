a3f68e18e752022cc44bc86d216e6640a9d82068
DorianSzeno <dorian.szenozicska@gmail.com>
2025-02-06 16:30:18 +0100

    frontend begin

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,6 @@
 import "@/styles/globals.css";
+import "@/styles/sidebar.css"
+import "@/styles/box.css"
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
